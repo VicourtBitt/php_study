6ad1a1d44e9bc9f6c926945714e842dfc77c674f
VicourtBitt <victorgcb@outlook.com>
2024-08-30 14:10:24 -0300

    Functions with ENUM and CustomTypes

--- typescript/compileTest/build/index.js
+++ typescript/compileTest/build/index.js
@@ -5,11 +5,25 @@ var Gender;
     Gender["Feminino"] = "Feminino";
     Gender["NaoBinario"] = "Nao-Binario";
 })(Gender || (Gender = {}));
-const student = {
+let studentList = [];
+function createStudent(info) {
+    if (!info) {
+        return;
+    }
+    studentList.push(info);
+}
+createStudent({
     firstName: 'Victor',
     lastName: 'Bittencourt',
     gender: Gender.Masculino,
     age: 20,
     roleAtTime: 'Student'
-};
-console.log(student);
+});
+createStudent({
+    firstName: 'Gabriel',
+    lastName: 'Mansolff',
+    gender: Gender.Masculino,
+    age: 18,
+    roleAtTime: 'Student'
+});
+console.log(studentList);
--- typescript/compileTest/index.ts
+++ typescript/compileTest/index.ts
@@ -12,12 +12,29 @@ type UserInfo = {
     roleAtTime?: string
 }
 
-const student : UserInfo = {
+let studentList : Array<UserInfo>= []
+
+function createStudent(info: UserInfo) {
+    if (!info) {
+        return
+    }
+    studentList.push(info)
+} 
+
+createStudent({
     firstName: 'Victor',
     lastName: 'Bittencourt',
     gender: Gender.Masculino,
     age: 20,
     roleAtTime: 'Student'
-}
+})
 
-console.log(student)
+createStudent({
+    firstName: 'Gabriel',
+    lastName: 'Mansolff',
+    gender: Gender.Masculino,
+    age: 18,
+    roleAtTime: 'Student'
+})
+
+console.log(studentList)
